6030cf6649532dff0f1984962f75f04014503ea6
Jens Nolte <git@queezle.net>
2021-08-19 01:29:08 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,30 +2,34 @@
   description = "Hello World!";
 
   inputs = {
-    quasar = {
-      url = gitlab:jens/quasar?host=git.c3pb.de;
-      inputs.nixpkgs.follows = "nixpkgs";
-      #follows = "quasar-network/quasar";
-    };
     quasar-network = {
       url = gitlab:jens/quasar-network?host=git.c3pb.de;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, quasar, quasar-network }:
+  outputs = { self, nixpkgs, quasar-network }:
   let
     lib = nixpkgs.lib;
     systems = lib.platforms.unix;
-    forAllSystems = f: lib.genAttrs systems (system: f system);
+    forAllSystems = lib.genAttrs systems;
   in {
     packages = forAllSystems (system: {
       q = import ./. {
-        pkgs = import nixpkgs { inherit system; overlays = [ quasar.overlay quasar-network.overlay ]; };
+        pkgs = import nixpkgs { inherit system; overlays = [ quasar-network.overlays.quasar quasar-network.overlay ]; };
       };
     });
 
-    overlay = self: super:  { q = import ./. { pkgs = self; }; };
+    overlay = final: prev: {
+      haskell = prev.haskell // {
+        packageOverrides = hfinal: hprev: prev.haskell.packageOverrides hfinal hprev // {
+          q = import ./. {
+            pkgs = final;
+            haskellPackages = hfinal;
+          };
+        };
+      };
+    };
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.q;
     devShell.x86_64-linux = self.defaultPackage.x86_64-linux.env;
